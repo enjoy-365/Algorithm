ed92abb945f904a4341f7113c49088f2364a3582
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-02-25 18:29:08 +0900

    heapsort complete

--- sorting/heapsort.c
+++ sorting/heapsort.c
@@ -23,12 +23,13 @@ struct HeapStruct {
 
 PriorityQueue InitHeap(int heapsize);
 void Insert(ElementType x, PriorityQueue h, FILE* fp2);
-void HeapSort(PriorityQueue h, int n);
-void MaxHeapify(PriorityQueue h, int i, int n);
+void HeapSort(PriorityQueue h, int cnt, FILE* fp2);
+void BuildHeap(PriorityQueue h);
+void MaxHeapify(PriorityQueue h, int i);
 int DeleteMax(PriorityQueue h, FILE* fp2);
 int Find(ElementType x, PriorityQueue h);
 //int FindRecursive(ElementType x, PriorityQueue h);
-void Print(PriorityQueue h, FILE* fp2);
+void Print(PriorityQueue h, int n, FILE* fp2);
 int IsFull(PriorityQueue h);
 int IsEmpty(PriorityQueue h);
 
@@ -42,29 +43,74 @@ int main() {
 	fscanf(fp1, "%d", &cnt);
 
 	PriorityQueue h = InitHeap(cnt);
+	PrintDebug("InitHeap is done\n");
 
 	for (int i = 1; i <= cnt; i++) {
 		fscanf(fp1, "%d", &n);
+
+		PrintDebug("insert %d\n", n);
+
 		h->elements[i] = n;
+		h->size++;
 	}
 
+	HeapSort(h, cnt, fp2);
 
-
-
+	fprintf(fp2, "after heapsort: ");
+	Print(h, cnt, fp2);
 
 	fclose(fp1);
 	fclose(fp2);
 
 	return 0;
 }
-void HeapSort(PriorityQueue h, int n) {
+void BuildHeap(PriorityQueue h) {
+	//O(N)
+	int i;
+	for (i = h->size / 2; i > 0; i--) {
+		MaxHeapify(h, i);
+	}
+}
+void MaxHeapify(PriorityQueue h, int i) {
+	int idx = i;
+	int left = 2 * i;
+	int right = left + 1;
+	if (left <= h->size && h->elements[left] > h->elements[idx]) {
+		idx = left;
+	}
+	if (right <= h->size && h->elements[right] > h->elements[idx]) {
+		idx = right;
+	}
+	if (idx != i) {
+		int maxValue = h->elements[idx];
+		h->elements[idx] = h->elements[i];
+		h->elements[i] = maxValue;
 
+		MaxHeapify(h, idx);
+	}
+}
+void HeapSort(PriorityQueue h, int cnt, FILE* fp2) {
+	BuildHeap(h);
+
+	fprintf(fp2, "after buildHeap: ");
+	Print(h, cnt, fp2);
+	fprintf(fp2, "\n");
+	int i;
+	for (i = h->size; i > 1; i--) {
+		int maxValue = h->elements[1];
+		h->elements[1] = h->elements[h->size];
+		h->elements[h->size] = maxValue;
+		h->size--;
+		MaxHeapify(h, 1);
+	}
 }
 PriorityQueue InitHeap(int heapsize) {
 	PriorityQueue h = (PriorityQueue)malloc(sizeof(struct HeapStruct));
-	h->capacity = heapsize;
+	h->capacity = heapsize + 1;
 	h->size = 0;
 	h->elements = (ElementType*)malloc(sizeof(int) * h->capacity);
+
+	return h;
 }
 void Insert(ElementType x, PriorityQueue h, FILE* fp2) {
 	if (IsFull(h)) {
@@ -86,6 +132,7 @@ ElementType DeleteMax(PriorityQueue h, FILE* fp2) {
 		return -1;
 	}
 
+	//find the right position for the last element.
 	ElementType maxElement = h->elements[1];
 	ElementType lastElement = h->elements[h->size--];
 
@@ -118,8 +165,8 @@ int Find(ElementType x, PriorityQueue h) {
 	}
 	return 0;
 }
-void Print(PriorityQueue h, FILE* fp2) {
-	for (int i = 1; i <= h->size; i++) {
+void Print(PriorityQueue h, int n, FILE* fp2) {
+	for (int i = 1; i <= n; i++) {
 		fprintf(fp2, "%d ", h->elements[i]);
 	}
 }
